a7a9844bfbb42ead4421ecbb658ee7f034e86d23
John Workmeister <jtworkme@us.ibm.com>
2016-02-25 11:29:56 -0500

    fixing copy file path

--- install-launcher.bat
+++ install-launcher.bat
@@ -4,8 +4,8 @@ mkdir %APPDATA%\first-discovery
 :MOVEFILES
 pushd %~dp0
 SET INSTALLDIR = %APPDATA%\first-discovery
-COPY %~dp0\launcher.bat %APPDATA%\first-discovery
-COPY %~dp0\globe-rgb.ico %APPDATA%\first-discovery
-COPY %~dp0\globe-bw.ico %APPDATA%\first-discovery
+COPY launcher.bat %APPDATA%\first-discovery
+COPY globe-rgb.ico %APPDATA%\first-discovery
+COPY globe-bw.ico %APPDATA%\first-discovery
 
 cscript create-shortcut.vbs
